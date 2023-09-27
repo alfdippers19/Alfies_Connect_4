16a5bf0c31eeb0be7a6455229ae86bda48ec699e
Alfie <142598509+alfdippers19@users.noreply.github.com>
2023-09-27 12:36:01 +0100

    Update Connect_4.py
    Added comments to make code clearer

--- Connect_4.py
+++ Connect_4.py
@@ -4,20 +4,20 @@ row3 = [' ',' ',' ',' ',' ',' ',' ']
 row2 = [' ',' ',' ',' ',' ',' ',' ']
 row1 = [' ',' ',' ',' ',' ',' ',' ']
 row0 = [' ',' ',' ',' ',' ',' ',' ']
-map = [row0,row1,row2,row3,row4,row5]
-done = False
-game_over = False
-player1_go = True
+map = [row0,row1,row2,row3,row4,row5] #This map represents the how the game would look in real life
+done = False 
+game_over = False #When this is true the game ends
+player1_go = True #This boolean is used to switch between player 1s go and player 2s go
 player1_chip = ""
-player2_chip = ""
-column_choice = 0
-player1_chip = input("""Player 1, would you like to be "X" or "O"?: """)
+player2_chip = "" #sets the variable type for these variables so there isnt a confusion with data types
+column_choice = 0 #same as before with setting variable types
+player1_chip = input("""Player 1, would you like to be "X" or "O"?: """) #sets the players 'chips'
 if player1_chip == "X":
     player2_chip = "O"
 else:
     player2_chip = "X"
-while game_over != True:
-    for x in range(0,7):
+while game_over != True: #Main game loop
+    for x in range(0,7): #prints the 'game board' so to speak
         if x == 6:
             print(f"  {x}\n")
         else:
@@ -34,7 +34,7 @@ while game_over != True:
             print(f"  \/",end=" ")
     for x in range(len(map),0,-1):
         print(f"{map[x-1]}\n")
-    if player1_go == True:
+    if player1_go == True: #if player 1s go is true he or she can place there chip in a column of their choice
         while done != True:
             column_choice = int(input("Player 1, which row do you choose?: "))
             if map[5][column_choice] != ' ':
@@ -50,9 +50,9 @@ while game_over != True:
                 map[3][column_choice] = player1_chip
                 map[4][column_choice] = ' '
                 if map[2][column_choice] == ' ':
-                    map[2][column_choice] = player1_chip
-                    map[3][column_choice] = ' '
-                    if map[1][column_choice] == ' ':
+                    map[2][column_choice] = player1_chip #this algorythm mimics how the chip would fall in real life, it checks for each level if the ->
+                    map[3][column_choice] = ' '          #-> space below is free, if it is the chip is placed and the one above chances to empty ->
+                    if map[1][column_choice] == ' ':     #-> (mimics motion of a falling chip)
                         map[1][column_choice] = player1_chip
                         map[2][column_choice] = ' '
                         if map[0][column_choice] == ' ':
@@ -67,7 +67,7 @@ while game_over != True:
             else:
                 done = True
         done = False
-        map[5][column_choice] = player2_chip
+        map[5][column_choice] = player2_chip #same thing happens if its player 2s go
         if map[4][column_choice] == ' ':
             map[4][column_choice] = player2_chip
             map[5][column_choice] = ' '
@@ -84,8 +84,8 @@ while game_over != True:
                             map[0][column_choice] = player2_chip
                             map[1][column_choice] = ' '
         player1_go = True
-    for y in range(0,6):
-        for x in range(0,4):
+    for y in range(0,6): #BIG checking algorythm
+        for x in range(0,4):#This one checks horizontaly
             if map[y][x] == "X":
                 if map[y][x+1] == "X":
                     if map[y][x+2] == "X":
@@ -106,7 +106,7 @@ while game_over != True:
                             elif player2_chip == "O":
                                 print("Player 2 wins!")
                                 game_over= True
-    for y in range(0,3):
+    for y in range(0,3): # checks vertically
         for x in range(0,7):
             if map[y][x] == "X":
                 if map[y+1][x] == "X":
@@ -128,7 +128,7 @@ while game_over != True:
                             elif player2_chip == "O":
                                 print("Player 2 wins!")
                                 game_over= True
-    for y in range(0,3):
+    for y in range(0,3): #checks diagonally with a positive gradient
         for x in range(0,4):
                 if map[y][x] == "X":
                     if map[y+1][x+1] == "X":
@@ -150,7 +150,7 @@ while game_over != True:
                                 elif player2_chip == "O":
                                     print("Player 2 wins!")
                                     game_over= True
-    for y in range(0,6):
+    for y in range(0,6): # checks diagonally with a negative gradient
         for x in range(0,4):
             if map[y][x] == "X":
                 if map[y-1][x+1] == "X":
@@ -172,7 +172,7 @@ while game_over != True:
                             elif player2_chip == "O":
                                 print("Player 2 wins!")
                                 game_over= True
-for x in range(0,7):
+for x in range(0,7): #Displays the end result once a game over is detected
     if x == 6:
         print(f"  {x}\n")
     else:
